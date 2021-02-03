78593a6663ada2c0b4d3215d95c86d19a5ad1af7
Mateusz <matyd356@gmail.com>
2021-02-03 21:45:55 +0100

    add mongoDB

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,8 +7,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mongoose = require('mongoose')
 
 var app = express();
+
+//database setup
+var mongoDB = process.env.MONGO_DB_LINK
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +32,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
