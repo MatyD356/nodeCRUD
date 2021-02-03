5cc64e5bd9f02bb806dd839833b9eb3f1f378c2a
Mateusz <matyd356@gmail.com>
2021-02-03 22:23:23 +0100

    add database models and populate them

---
+++ models/genre.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+
+var Genre = new Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 100 },
+
+})
+
+Genre
+  .virtual('url')
+  .get(() => {
+    return '/catalog/genre/' + this._id;
+  });
+
+
+module.exports = mongoose.model('Genre', Genre)
